d5b6be9b5d027ef1b3dd486e8f507168150ef0e2
Matvey Eikholts <matze85eikholts@gmail.com>
2024-01-08 06:36:01 +0300

    training of loops

---
+++ 11-python-workout-papers/01-loops-learn workout.py
@@ -0,0 +1,32 @@
+
+for x in range(1, 10):
+  print(x)
+
+#1 3 5 7 9
+for x in range(1, 10, 2):
+  print(x)
+
+#2 4 6 8
+for x in range(2, 10, 2):
+  print(x)
+
+# 10, 8, 6, 4, 2
+for x in range(10, 1, -2):
+  print(x)
+
+
+#for-each kind
+fruits = ["apple", "banana", "tomato"]
+for fruit in fruits:
+  print(fruit)
+
+#---training 10 set by 10 times
+fruits_set_1 = ["apple", "banana", "juice"]
+for fruit in fruits_set_1:
+  print(fruit)
+
+  
+#print(fruits_set_1.len())
+#list has no attribute length
+
+
