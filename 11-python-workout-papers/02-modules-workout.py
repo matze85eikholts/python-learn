dc6d74774a6c606fdb7d04396e8b937a6e4b40b0
Matvey Eikholts <matze85eikholts@gmail.com>
2024-01-09 16:46:41 +0300

    Modules in python repeated

---
+++ 11-python-workout-papers/02-modules-workout.py
@@ -0,0 +1,7 @@
+import orange
+
+orange.get_orange()
+
+orange.get_orange()
+
+orange.eat_orange()
---
+++ 11-python-workout-papers/orange.py
@@ -0,0 +1,5 @@
+def get_orange():
+  print("Hello from the hot orange!!!")
+
+def eat_orange():
+  print("You can eat me now!!")
